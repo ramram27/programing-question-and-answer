88148a230afc6def46f59b869666178a8911649f
Rohit Kumar <54281438+ramram27@users.noreply.github.com>
2021-12-07 23:31:30 +0530

    code in reverse string

--- Reverse String.js
+++ Reverse String.js
@@ -1,1 +1,21 @@
+var str="Share Javascript code online using";
+var strArr = str.split(" ");
 
+var revStr = "";
+
+for(let i=0;i<strArr.length;i++) {
+    var x = strArr[i];
+    
+    var charArr=x.split("").reverse();
+    var charStr="";
+    
+    for(let j=0;j<charArr.length;j++){
+      
+      charStr += charArr[j];
+     
+    }
+    
+    revStr += charStr + " ";
+}
+
+console.log(revStr);
